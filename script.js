c77bf0b46e38feafa5ddad8dc14066581365ab39
Tim <118667807+MrPrimeHD@users.noreply.github.com>
2023-02-19 01:03:30 +0100

    You can choose now what the length of your password should be, and it is possible to copy the password by clicking on the button

--- script.js
+++ script.js
@@ -3,19 +3,29 @@ const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","
 let generateButton = document.getElementById("genBtn");
 let firstPassword = document.getElementById("password1");
 let secondPassword = document.getElementById("password2");
+let lengthOfPassword = 0;
+
 function generatePassword() {
     let password = "";
-    for (let i = 0; i < 16; i++) {
-        password += characters[Math.floor(Math.random() * characters.length)];
+    if(lengthOfPassword > 25 || lengthOfPassword < 1) {
+        alert("Password length must be between 1 and 25 characters");
+    } else {
+        for (let i = 0; i <= lengthOfPassword; i++) {
+            password += characters[Math.floor(Math.random() * characters.length)];
+        }
+
     }
     return password;
 }
 
 function renderPassword() {
+    lengthOfPassword = document.getElementById("lengthOfPassword").value;
     firstPassword.textContent = generatePassword();
     secondPassword.textContent = generatePassword();
 }
 
-
-
-
+//Copy the text of the button to the clipboard
+function copyToClipboard(id) {
+    let text = document.getElementById(id);
+    navigator.clipboard.writeText(text.textContent);
+}
